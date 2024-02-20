8ff4a3962add90899d939ef8c59ec0150903254a
Frank Xu <frank.xu@northbright.com>
2024-02-20 11:45:36 +0800

    Add parseDirWithDelims to make code shorter

--- templatehelper.go
+++ templatehelper.go
@@ -13,16 +13,23 @@ const (
 	DefTmplExt = ".tmpl"
 )
 
-// ParseDirWithDelims parses all template files in the given dir and subdirs recursively.
+// parseDirWithDelims parses all template files in the named dir and subdirs recursively.
 // It returns a slice contains parsed templates.
 // The name of each parsed template is set to the relative path of the template file.
 // The path contains the "dir" argument as a prefix.
+// fsys: The file system(fs.FS) contains the named dir.
+// Set it to nil if dir is not in a fs.FS.
 // dir: the dir contains template files.
 // ext: extend name of template file(e.g. ".tmpl"). It'll use ".tmpl" as default extend name if ext is empty.
 // leftDelim / rightDelim: left / right delimiter of the template.
 // It'll use default delimiters("{{" and "}}") of Golang if any of them is empty.
-func ParseDirWithDelims(dir, ext, leftDelim, rightDelim string) ([]*template.Template, error) {
-	var tmpls []*template.Template
+func parseDirWithDelims(fsys fs.FS, dir, ext, leftDelim, rightDelim string) ([]*template.Template, error) {
+	var (
+		err     error
+		tmpls   []*template.Template
+		entries []fs.DirEntry
+		data    []byte
+	)
 
 	// Set ext to default template ext if it's empty.
 	if ext == "" {
@@ -30,17 +37,29 @@ func ParseDirWithDelims(dir, ext, leftDelim, rightDelim string) ([]*template.Tem
 	}
 	ext = strings.ToLower(ext)
 
-	entries, err := os.ReadDir(dir)
+	if fsys != nil {
+		// dir is in a fs.FS.
+		entries, err = fs.ReadDir(fsys, dir)
+	} else {
+		// dir is not in a fs.FS.
+		entries, err = os.ReadDir(dir)
+	}
+
 	if err != nil {
 		return nil, err
 	}
 
 	for _, entry := range entries {
 		if entry.IsDir() {
-			// Parse templates in sub dir recursively.
-			subDir := filepath.Join(dir, entry.Name())
+			subDir := ""
+			if fsys != nil {
+				subDir = path.Join(dir, entry.Name())
+			} else {
+				subDir = filepath.Join(dir, entry.Name())
+			}
 
-			tmplsInSubDir, err := ParseDirWithDelims(subDir, ext, leftDelim, rightDelim)
+			// Parse templates in sub dir recursively.
+			tmplsInSubDir, err := parseDirWithDelims(fsys, subDir, ext, leftDelim, rightDelim)
 			if err != nil {
 				return nil, err
 			}
@@ -53,10 +72,24 @@ func ParseDirWithDelims(dir, ext, leftDelim, rightDelim string) ([]*template.Tem
 				continue
 			}
 
-			path := filepath.Join(dir, filename)
+			p := ""
 
-			// Read the content from the template file.
-			data, err := os.ReadFile(path)
+			if fsys != nil {
+				// dir is in a fs.FS:
+				// always use forward slash('/') as path separtor.
+				p = path.Join(dir, filename)
+
+				// Read the content from the template file.
+				data, err = fs.ReadFile(fsys, p)
+			} else {
+				// dir is not in a fs.FS:
+				// use OS specified path separator.
+				p = filepath.Join(dir, filename)
+
+				// Read the content from the template file.
+				data, err = os.ReadFile(p)
+			}
+
 			if err != nil {
 				return nil, err
 			}
@@ -68,7 +101,7 @@ func ParseDirWithDelims(dir, ext, leftDelim, rightDelim string) ([]*template.Tem
 			}
 
 			// Create a new empty template which name is path.
-			t := template.New(path)
+			t := template.New(p)
 
 			// Set delimiters if need.
 			if leftDelim != "" && rightDelim != "" {
@@ -85,6 +118,18 @@ func ParseDirWithDelims(dir, ext, leftDelim, rightDelim string) ([]*template.Tem
 	}
 
 	return tmpls, nil
+}
+
+// ParseDirWithDelims parses all template files in the given dir and subdirs recursively.
+// It returns a slice contains parsed templates.
+// The name of each parsed template is set to the relative path of the template file.
+// The path contains the "dir" argument as a prefix.
+// dir: the dir contains template files.
+// ext: extend name of template file(e.g. ".tmpl"). It'll use ".tmpl" as default extend name if ext is empty.
+// leftDelim / rightDelim: left / right delimiter of the template.
+// It'll use default delimiters("{{" and "}}") of Golang if any of them is empty.
+func ParseDirWithDelims(dir, ext, leftDelim, rightDelim string) ([]*template.Template, error) {
+	return parseDirWithDelims(nil, dir, ext, leftDelim, rightDelim)
 }
 
 // ParseDir parses all template files in the given dir and subdirs recursively.
@@ -107,72 +152,7 @@ func ParseDir(dir, ext string) ([]*template.Template, error) {
 // leftDelim / rightDelim: left / right delimiter of the template.
 // It'll use default delimiters("{{" and "}}") of Golang if any of them is empty.
 func ParseFSDirWithDelims(fsys fs.FS, dir, ext, leftDelim, rightDelim string) ([]*template.Template, error) {
-	var tmpls []*template.Template
-
-	// Set ext to default template ext if it's empty.
-	if ext == "" {
-		ext = DefTmplExt
-	}
-	ext = strings.ToLower(ext)
-
-	entries, err := fs.ReadDir(fsys, dir)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, entry := range entries {
-		if entry.IsDir() {
-			// Parse templates in sub dir recursively.
-			subDir := path.Join(dir, entry.Name())
-
-			tmplsInSubDir, err := ParseFSDirWithDelims(fsys, subDir, ext, leftDelim, rightDelim)
-			if err != nil {
-				return nil, err
-			}
-
-			tmpls = append(tmpls, tmplsInSubDir...)
-		} else {
-			filename := entry.Name()
-
-			if strings.ToLower(filepath.Ext(filename)) != ext {
-				continue
-			}
-
-			path := path.Join(dir, filename)
-			if err != nil {
-				return nil, err
-			}
-
-			// Read the content from the template file.
-			data, err := fs.ReadFile(fsys, path)
-			if err != nil {
-				return nil, err
-			}
-
-			// Convert content from []byte to string via strings.Builder.
-			var b strings.Builder
-			if _, err = b.Write(data); err != nil {
-				return nil, err
-			}
-
-			// Create a new empty template which name is path.
-			t := template.New(path)
-
-			// Set delimiters if need.
-			if leftDelim != "" && rightDelim != "" {
-				t = t.Delims(leftDelim, rightDelim)
-			}
-
-			// Parse the template.
-			if t, err = t.Parse(b.String()); err != nil {
-				return nil, err
-			}
-
-			tmpls = append(tmpls, t)
-		}
-	}
-
-	return tmpls, nil
+	return parseDirWithDelims(fsys, dir, ext, leftDelim, rightDelim)
 }
 
 // ParseFSDir parses all template files in the file system and named dir recursively.
